7763bae97f1c5777701328e0c8df518eb1a16520
subbu7624 <93445771+subbu7624@users.noreply.github.com>
2021-11-22 12:58:15 -0500

    Add files via upload

---
+++ src/main/java/index.java
@@ -0,0 +1,9 @@
+public class index
+ {
+	 public static void main(String args[])
+	 {
+		 System.out.println("Welcome to project1.0 Release..");
+                 System.out.println("Welcome to project1.0 update 1 ...");
+
+	}
+}
